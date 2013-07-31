a47edaa17064f8f187ea5b6e800bd36b43236f9f
Sebastian Walla <sebastian.walla@gmx.de>
2013-07-31 09:26:24 +0200

    Added the current code

--- src/com/example/s_crewclientapp/MainActivity.java
+++ src/com/example/s_crewclientapp/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.s_crewclientapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 
 	@Override
@@ -18,5 +23,53 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+   public void setBestellung() throws IOException{
+	  //variable deklaration
+	   Socket serverside = null;
+	  
+	   try {
+		serverside=new Socket("localhost",5555);//serverside ist der server
+	} catch (UnknownHostException e) {
+		CharSequence text;
+		text = (CharSequence) e;
+		Toast toast = Toast.makeText(getApplicationContext(),text, 10);
+				toast.show();
+		
+		// TODO Auto-generated catch block
+		
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		CharSequence text;
+		text = (CharSequence) e;
+		Toast toast = Toast.makeText(getApplicationContext(),text, 10);
+				toast.show();
+		
 
+	}
+	//Ende try catch
+	   
+	   ObjectInputStream obi=(ObjectInputStream) serverside.getInputStream();
+	 OutputStream o= serverside.getOutputStream();
+	 Object server;
+	 try {
+		server = obi.readObject();
+	} catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		CharSequence text;
+		text = (CharSequence) e;
+		Toast toast = Toast.makeText(getApplicationContext(),text, 10);
+				toast.show();
+	}
+	//Ende Try catch
+	
+	 
+	 server.//Essensplan= noch davor schreiben aber vorher Essensplanklasse deklarieren :)	
+	 
+
+	
+	//finally
+	serverside.close();
+	 
+	   
+   }
 }
